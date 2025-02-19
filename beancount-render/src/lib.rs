e992e8078b7f36f42e7395b199b29c12f72f5e5e
James Mayclin <jam.mayc@gmail.com>
2025-02-18 23:42:08 -0800

    fix(render): remove lifetimes
    I think I'd prefer all of the render functionality to just live in a
    separate module in the core? I can't quite explain why, but this feels
    like too many crates.

--- beancount-render/src/lib.rs
+++ beancount-render/src/lib.rs
@@ -17,7 +17,7 @@ impl BasicRenderer {
     }
 }
 
-pub fn render<W: Write>(w: &mut W, ledger: &Ledger<'_>) -> Result<(), BasicRendererError> {
+pub fn render<W: Write>(w: &mut W, ledger: &Ledger) -> Result<(), BasicRendererError> {
     BasicRenderer::default().render(ledger, w)
 }
 
@@ -34,9 +34,9 @@ pub trait Renderer<T, W: Write> {
     fn render(&self, renderable: T, write: &mut W) -> Result<(), Self::Error>;
 }
 
-impl<'a, W: Write> Renderer<&'a Ledger<'_>, W> for BasicRenderer {
+impl<'a, W: Write> Renderer<&'a Ledger, W> for BasicRenderer {
     type Error = BasicRendererError;
-    fn render(&self, ledger: &'a Ledger<'_>, write: &mut W) -> Result<(), Self::Error> {
+    fn render(&self, ledger: &'a Ledger, write: &mut W) -> Result<(), Self::Error> {
         for directive in &ledger.directives {
             self.render(directive, write)?;
             writeln!(write)?;
@@ -45,9 +45,9 @@ impl<'a, W: Write> Renderer<&'a Ledger<'_>, W> for BasicRenderer {
     }
 }
 
-impl<'a, W: Write> Renderer<&'a Document<'_>, W> for BasicRenderer {
+impl<'a, W: Write> Renderer<&'a Document, W> for BasicRenderer {
     type Error = BasicRendererError;
-    fn render(&self, document: &'a Document<'_>, write: &mut W) -> Result<(), Self::Error> {
+    fn render(&self, document: &'a Document, write: &mut W) -> Result<(), Self::Error> {
         // TODO: Tags? Links?
         write!(write, "{} document ", document.date)?;
         self.render(&document.account, write)?;
@@ -57,9 +57,9 @@ impl<'a, W: Write> Renderer<&'a Document<'_>, W> for BasicRenderer {
     }
 }
 
-impl<'a, W: Write> Renderer<&'a Directive<'_>, W> for BasicRenderer {
+impl<'a, W: Write> Renderer<&'a Directive, W> for BasicRenderer {
     type Error = BasicRendererError;
-    fn render(&self, directive: &'a Directive<'_>, write: &mut W) -> Result<(), Self::Error> {
+    fn render(&self, directive: &'a Directive, write: &mut W) -> Result<(), Self::Error> {
         use Directive::*;
         match directive {
             Open(open) => self.render(open, write),
@@ -85,7 +85,7 @@ impl<'a, W: Write> Renderer<&'a Directive<'_>, W> for BasicRenderer {
 fn render_key_value<W: Write>(
     renderer: &BasicRenderer,
     w: &mut W,
-    kv: &HashMap<Cow<'_, str>, MetaValue<'_>>,
+    kv: &HashMap<String, MetaValue>,
 ) -> Result<(), BasicRendererError> {
     for (key, value) in kv {
         write!(w, "\t{}: ", key)?;
@@ -95,9 +95,9 @@ fn render_key_value<W: Write>(
     Ok(())
 }
 
-impl<'a, W: Write> Renderer<&'a MetaValue<'_>, W> for BasicRenderer {
+impl<'a, W: Write> Renderer<&'a MetaValue, W> for BasicRenderer {
     type Error = BasicRendererError;
-    fn render(&self, mv: &'a MetaValue<'_>, w: &mut W) -> Result<(), Self::Error> {
+    fn render(&self, mv: &'a MetaValue, w: &mut W) -> Result<(), Self::Error> {
         match mv {
             MetaValue::Account(account) => self.render(account, w)?,
             MetaValue::Amount(amount) => self.render(amount, w)?,
@@ -112,9 +112,9 @@ impl<'a, W: Write> Renderer<&'a MetaValue<'_>, W> for BasicRenderer {
     }
 }
 
-impl<'a, W: Write> Renderer<&'a Open<'_>, W> for BasicRenderer {
+impl<'a, W: Write> Renderer<&'a Open, W> for BasicRenderer {
     type Error = BasicRendererError;
-    fn render(&self, open: &'a Open<'_>, write: &mut W) -> Result<(), Self::Error> {
+    fn render(&self, open: &'a Open, write: &mut W) -> Result<(), Self::Error> {
         write!(write, "{} open ", open.date)?;
         self.render(&open.account, write)?;
         for currency in open.currencies.iter() {
@@ -135,9 +135,9 @@ impl<'a, W: Write> Renderer<&'a Open<'_>, W> for BasicRenderer {
     }
 }
 
-impl<'a, W: Write> Renderer<&'a Close<'_>, W> for BasicRenderer {
+impl<'a, W: Write> Renderer<&'a Close, W> for BasicRenderer {
     type Error = BasicRendererError;
-    fn render(&self, close: &'a Close<'_>, write: &mut W) -> Result<(), Self::Error> {
+    fn render(&self, close: &'a Close, write: &mut W) -> Result<(), Self::Error> {
         write!(write, "{} close ", close.date)?;
         self.render(&close.account, write)?;
         writeln!(write)?;
@@ -146,9 +146,9 @@ impl<'a, W: Write> Renderer<&'a Close<'_>, W> for BasicRenderer {
     }
 }
 
-impl<'a, W: Write> Renderer<&'a Account<'_>, W> for BasicRenderer {
+impl<'a, W: Write> Renderer<&'a Account, W> for BasicRenderer {
     type Error = BasicRendererError;
-    fn render(&self, account: &'a Account<'_>, write: &mut W) -> Result<(), Self::Error> {
+    fn render(&self, account: &'a Account, write: &mut W) -> Result<(), Self::Error> {
         write!(
             write,
             "{}:{}",
@@ -159,9 +159,9 @@ impl<'a, W: Write> Renderer<&'a Account<'_>, W> for BasicRenderer {
     }
 }
 
-impl<'a, W: Write> Renderer<&'a Balance<'_>, W> for BasicRenderer {
+impl<'a, W: Write> Renderer<&'a Balance, W> for BasicRenderer {
     type Error = BasicRendererError;
-    fn render(&self, balance: &'a Balance<'_>, w: &mut W) -> Result<(), Self::Error> {
+    fn render(&self, balance: &'a Balance, w: &mut W) -> Result<(), Self::Error> {
         write!(w, "{} balance ", balance.date)?;
         self.render(&balance.account, w)?;
         write!(w, "\t")?;
@@ -175,33 +175,33 @@ impl<'a, W: Write> Renderer<&'a Balance<'_>, W> for BasicRenderer {
     }
 }
 
-impl<'a, W: Write> Renderer<&'a Amount<'_>, W> for BasicRenderer {
+impl<'a, W: Write> Renderer<&'a Amount, W> for BasicRenderer {
     type Error = BasicRendererError;
-    fn render(&self, amount: &'a Amount<'_>, w: &mut W) -> Result<(), Self::Error> {
+    fn render(&self, amount: &'a Amount, w: &mut W) -> Result<(), Self::Error> {
         write!(w, "{} {}", amount.num, amount.currency)?;
         Ok(())
     }
 }
 
-impl<'a, W: Write> Renderer<&'a BcOption<'_>, W> for BasicRenderer {
+impl<'a, W: Write> Renderer<&'a BcOption, W> for BasicRenderer {
     type Error = BasicRendererError;
-    fn render(&self, option: &'a BcOption<'_>, w: &mut W) -> Result<(), Self::Error> {
+    fn render(&self, option: &'a BcOption, w: &mut W) -> Result<(), Self::Error> {
         writeln!(w, "option \"{}\" \"{}\"", option.name, option.val)?;
         Ok(())
     }
 }
 
-impl<'a, W: Write> Renderer<&'a Commodity<'_>, W> for BasicRenderer {
+impl<'a, W: Write> Renderer<&'a Commodity, W> for BasicRenderer {
     type Error = BasicRendererError;
-    fn render(&self, commodity: &'a Commodity<'_>, w: &mut W) -> Result<(), Self::Error> {
+    fn render(&self, commodity: &'a Commodity, w: &mut W) -> Result<(), Self::Error> {
         writeln!(w, "{} commodity {}", commodity.date, commodity.name)?;
         render_key_value(self, w, &commodity.meta)
     }
 }
 
-impl<'a, W: Write> Renderer<&'a Custom<'_>, W> for BasicRenderer {
+impl<'a, W: Write> Renderer<&'a Custom, W> for BasicRenderer {
     type Error = BasicRendererError;
-    fn render(&self, custom: &'a Custom<'_>, w: &mut W) -> Result<(), Self::Error> {
+    fn render(&self, custom: &'a Custom, w: &mut W) -> Result<(), Self::Error> {
         write!(
             w,
             "{} custom \"{}\" {}",
@@ -214,9 +214,9 @@ impl<'a, W: Write> Renderer<&'a Custom<'_>, W> for BasicRenderer {
     }
 }
 
-impl<'a, W: Write> Renderer<&'a Event<'_>, W> for BasicRenderer {
+impl<'a, W: Write> Renderer<&'a Event, W> for BasicRenderer {
     type Error = BasicRendererError;
-    fn render(&self, event: &'a Event<'_>, w: &mut W) -> Result<(), Self::Error> {
+    fn render(&self, event: &'a Event, w: &mut W) -> Result<(), Self::Error> {
         writeln!(
             w,
             "{} event \"{}\" \"{}\"",
@@ -226,17 +226,17 @@ impl<'a, W: Write> Renderer<&'a Event<'_>, W> for BasicRenderer {
     }
 }
 
-impl<'a, W: Write> Renderer<&'a Include<'_>, W> for BasicRenderer {
+impl<'a, W: Write> Renderer<&'a Include, W> for BasicRenderer {
     type Error = BasicRendererError;
-    fn render(&self, include: &'a Include<'_>, w: &mut W) -> Result<(), Self::Error> {
+    fn render(&self, include: &'a Include, w: &mut W) -> Result<(), Self::Error> {
         writeln!(w, "include \"{}\'", include.filename)?;
         Ok(())
     }
 }
 
-impl<'a, W: Write> Renderer<&'a Note<'_>, W> for BasicRenderer {
+impl<'a, W: Write> Renderer<&'a Note, W> for BasicRenderer {
     type Error = BasicRendererError;
-    fn render(&self, note: &'a Note<'_>, w: &mut W) -> Result<(), Self::Error> {
+    fn render(&self, note: &'a Note, w: &mut W) -> Result<(), Self::Error> {
         write!(w, "{} note ", note.date)?;
         self.render(&note.account, w)?;
         writeln!(w, " {}", note.comment)?;
@@ -244,9 +244,9 @@ impl<'a, W: Write> Renderer<&'a Note<'_>, W> for BasicRenderer {
     }
 }
 
-impl<'a, W: Write> Renderer<&'a Pad<'_>, W> for BasicRenderer {
+impl<'a, W: Write> Renderer<&'a Pad, W> for BasicRenderer {
     type Error = BasicRendererError;
-    fn render(&self, pad: &'a Pad<'_>, w: &mut W) -> Result<(), Self::Error> {
+    fn render(&self, pad: &'a Pad, w: &mut W) -> Result<(), Self::Error> {
         write!(w, "{} pad ", pad.date)?;
         self.render(&pad.pad_to_account, w)?;
         write!(w, " ")?;
@@ -256,9 +256,9 @@ impl<'a, W: Write> Renderer<&'a Pad<'_>, W> for BasicRenderer {
     }
 }
 
-impl<'a, W: Write> Renderer<&'a Plugin<'_>, W> for BasicRenderer {
+impl<'a, W: Write> Renderer<&'a Plugin, W> for BasicRenderer {
     type Error = BasicRendererError;
-    fn render(&self, plugin: &'a Plugin<'_>, w: &mut W) -> Result<(), Self::Error> {
+    fn render(&self, plugin: &'a Plugin, w: &mut W) -> Result<(), Self::Error> {
         write!(w, "plugin \"{}\"", plugin.module)?;
         if let Some(config) = &plugin.config {
             write!(w, " \"{}\"", config)?;
@@ -268,9 +268,9 @@ impl<'a, W: Write> Renderer<&'a Plugin<'_>, W> for BasicRenderer {
     }
 }
 
-impl<'a, W: Write> Renderer<&'a Price<'_>, W> for BasicRenderer {
+impl<'a, W: Write> Renderer<&'a Price, W> for BasicRenderer {
     type Error = BasicRendererError;
-    fn render(&self, price: &'a Price<'_>, w: &mut W) -> Result<(), Self::Error> {
+    fn render(&self, price: &'a Price, w: &mut W) -> Result<(), Self::Error> {
         write!(w, "{} price {} ", price.date, price.currency)?;
         self.render(&price.amount, w)?;
         writeln!(w)?;
@@ -278,9 +278,9 @@ impl<'a, W: Write> Renderer<&'a Price<'_>, W> for BasicRenderer {
     }
 }
 
-impl<'a, W: Write> Renderer<&'a Query<'_>, W> for BasicRenderer {
+impl<'a, W: Write> Renderer<&'a Query, W> for BasicRenderer {
     type Error = BasicRendererError;
-    fn render(&self, query: &'a Query<'_>, w: &mut W) -> Result<(), Self::Error> {
+    fn render(&self, query: &'a Query, w: &mut W) -> Result<(), Self::Error> {
         writeln!(
             w,
             "{} query \"{}\" \"{}\"",
@@ -290,9 +290,9 @@ impl<'a, W: Write> Renderer<&'a Query<'_>, W> for BasicRenderer {
     }
 }
 
-impl<'a, W: Write> Renderer<&'a Transaction<'_>, W> for BasicRenderer {
+impl<'a, W: Write> Renderer<&'a Transaction, W> for BasicRenderer {
     type Error = BasicRendererError;
-    fn render(&self, transaction: &'a Transaction<'_>, w: &mut W) -> Result<(), Self::Error> {
+    fn render(&self, transaction: &'a Transaction, w: &mut W) -> Result<(), Self::Error> {
         write!(w, "{} {}", transaction.date, transaction.flag)?;
         if let Some(payee) = &transaction.payee {
             write!(w, " \"{}\"", payee)?;
@@ -312,9 +312,9 @@ impl<'a, W: Write> Renderer<&'a Transaction<'_>, W> for BasicRenderer {
     }
 }
 
-impl<'a, W: Write> Renderer<&'a Posting<'_>, W> for BasicRenderer {
+impl<'a, W: Write> Renderer<&'a Posting, W> for BasicRenderer {
     type Error = BasicRendererError;
-    fn render(&self, posting: &'a Posting<'_>, w: &mut W) -> Result<(), Self::Error> {
+    fn render(&self, posting: &'a Posting, w: &mut W) -> Result<(), Self::Error> {
         write!(w, "\t")?;
         if let Some(flag) = &posting.flag {
             write!(w, "{} ", flag)?;
@@ -335,9 +335,9 @@ impl<'a, W: Write> Renderer<&'a Posting<'_>, W> for BasicRenderer {
     }
 }
 
-impl<'a, W: Write> Renderer<&'a CostSpec<'_>, W> for BasicRenderer {
+impl<'a, W: Write> Renderer<&'a CostSpec, W> for BasicRenderer {
     type Error = BasicRendererError;
-    fn render(&self, cost: &'a CostSpec<'_>, w: &mut W) -> Result<(), Self::Error> {
+    fn render(&self, cost: &'a CostSpec, w: &mut W) -> Result<(), Self::Error> {
         let double_brackets = cost.number_total.is_some();
         if double_brackets {
             write!(w, "{{{{")?;
@@ -377,9 +377,9 @@ impl<'a, W: Write> Renderer<&'a CostSpec<'_>, W> for BasicRenderer {
     }
 }
 
-impl<'a, W: Write> Renderer<&'a PriceSpec<'_>, W> for BasicRenderer {
+impl<'a, W: Write> Renderer<&'a PriceSpec, W> for BasicRenderer {
     type Error = BasicRendererError;
-    fn render(&self, price: &'a PriceSpec<'_>, w: &mut W) -> Result<(), Self::Error> {
+    fn render(&self, price: &'a PriceSpec, w: &mut W) -> Result<(), Self::Error> {
         let amount = match price {
             PriceSpec::PerUnit(amount) => {
                 write!(w, "@ ")?;
@@ -394,11 +394,11 @@ impl<'a, W: Write> Renderer<&'a PriceSpec<'_>, W> for BasicRenderer {
     }
 }
 
-impl<'a, W: Write> Renderer<&'a IncompleteAmount<'_>, W> for BasicRenderer {
+impl<'a, W: Write> Renderer<&'a IncompleteAmount, W> for BasicRenderer {
     type Error = BasicRendererError;
     fn render(
         &self,
-        incomplete_amount: &'a IncompleteAmount<'_>,
+        incomplete_amount: &'a IncompleteAmount,
         w: &mut W,
     ) -> Result<(), Self::Error> {
         match (&incomplete_amount.num, &incomplete_amount.currency) {
